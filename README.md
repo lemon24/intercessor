52caa602997326cbcab3d0395704f6771245dd71
Adrian Damian <damian.adrian24@gmail.com>
2017-06-19 23:42:34 +0300

    Initial commit (skeleton).

---
+++ README.md
@@ -0,0 +1,5 @@
+**intercessor** is a tool that runs Python scripts incrementally (something like Jupyter Notebook, but text-based).
+
+Development should follow a [problem-solution][] approach.
+
+[problem-solution]: https://hintjens.gitbooks.io/scalable-c/content/chapter1.html#problem-what-do-we-do-next
---
+++ setup.py
@@ -0,0 +1,24 @@
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('intercessor/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='intercessor',
+    version=version,
+    author='lemon24',
+    url='https://github.com/lemon24/intercessor',
+    packages=['intercessor'],
+    description="",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
+
