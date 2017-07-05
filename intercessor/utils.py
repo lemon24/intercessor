b6b73ebf3a009f1287b0280bdd43c676b50d5b37
Adrian Damian <damian.adrian24@gmail.com>
2017-07-06 01:12:24 +0300

    Add missing utils.py.

---
+++ intercessor/utils.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+
+try:
+    input = raw_input
+except NameError:
+    input = input
+
+
+def prompt(text):
+    try:
+        return input(text)
+    except EOFError:
+        print()
+        raise
+    except KeyboardInterrupt:
+        print('^C')
+        raise
+
+
+def confirm(text, default=False):
+    options = "Y/n" if default else "y/N"
+    try:
+        rv = input("{} [{}]: ".format(text, options))
+        rv = rv.strip().lower()
+        if not rv:
+            return default
+        return rv == 'y'
+    except EOFError:
+        print('y' if default else 'n')
+        return default
+    except KeyboardInterrupt:
+        print('^C')
+        return not default
+
